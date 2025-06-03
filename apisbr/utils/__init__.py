04ea308e049627ff092af96a422ccf3faca89b91
Gabriel Martins <gbielsesc@gmail.com>
2025-06-03 19:06:15 -0300

    Nomenclatura de UFs Adicionadas funções de utilidade para lidar com nomes e siglas/abreviações de unidades federativas.
    Signed-off-by: Gabriel Martins <gbielsesc@gmail.com>

--- apisbr/utils/__init__.py
+++ apisbr/utils/__init__.py
@@ -1,2 +1,3 @@
 from .basic_functions import *
 from .text_format_funcs import *
+from .uf_naming_funcs import *
---
+++ apisbr/utils/uf_naming_funcs.py
@@ -0,0 +1,68 @@
+from .text_format_funcs import remove_accents
+from .basic_functions import invert_dict
+
+_uf_name2abbrev ={
+    'Acre': 'AC',
+    'Alagoas': 'AL',
+    'Amapa': 'AP',
+    'Amazonas': 'AM',
+    'Bahia': 'BA',
+    'Ceara': 'CE',
+    'Distrito Federal': 'DF',
+    'Espirito Santo': 'ES',
+    'Goias': 'GO',
+    'Maranhao': 'MA',
+    'Mato Grosso': 'MT',
+    'Mato Grosso do Sul': 'MS',
+    'Minas Gerais': 'MG',
+    'Para': 'PA',
+    'Paraiba': 'PB',
+    'Parana': 'PR',
+    'Pernambuco': 'PE',
+    'Piaui': 'PI',
+    'Rio de Janeiro': 'RJ',
+    'Rio Grande do Norte': 'RN',
+    'Rio Grande do Sul': 'RS',
+    'Rondonia': 'RO',
+    'Roraima': 'RR',
+    'Santa Catarina': 'SC',
+    'Sao Paulo': 'SP',
+    'Sergipe': 'SE',
+    'Tocantins': 'TO'
+}
+
+_uf_abbrev2name = invert_dict(_uf_name2abbrev)
+
+def uf_name_to_abbrev(uf_name: str) -> str:
+    """
+    Recebe o nome de uma UF e retorna sua abreviação (SC, MT, DF...).
+
+    Parameters
+    ----------
+    uf_name : str
+        Nome da UF, com ou sem acento, com ou sem letras maiúsculas.
+
+    Returns
+    -------
+    str
+        Sigla referente à UF em letras maiúsculas.
+    """
+    name = remove_accents(uf_name).title()
+    return _uf_name2abbrev[name]
+
+def uf_abbrev_to_name(uf_abbrev: str) -> str:
+    """
+    Recebe a abreviação de uma UF (SC, MT, DF...) e retorna seu nome.
+
+    Parameters
+    ----------
+    uf_abbrev : str
+        Abreviação da UF, com ou sem letras maiúsculas.
+
+    Returns
+    -------
+    str
+        Nome da UF com palavras capitalizadas e sem acento (Sao Paulo, Maranhao...).
+    """
+    abbrev = uf_abbrev.upper()
+    return _uf_abbrev2name[abbrev]
